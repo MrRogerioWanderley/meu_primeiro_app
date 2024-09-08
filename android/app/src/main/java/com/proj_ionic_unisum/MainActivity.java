0959ad535b67687af3eee5430e553defe5644b6b
rogerio <rogeriowanderley@gmail.com>
2024-09-07 23:51:25 -0300

    finalização do projeto

---
+++ android/app/src/main/java/com/proj_ionic_unisum/MainActivity.java
@@ -0,0 +1,5 @@
+package com.proj_ionic_unisum;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.proj_ionic_unisum',
+  appName: 'proj_ionic_unisum',
+  webDir: 'www/browser'
+};
+
+export default config;
